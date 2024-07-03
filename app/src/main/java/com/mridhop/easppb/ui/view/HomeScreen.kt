3e44d018beaf15671f7db7589460e8decaaf9740
Ridho Pratama <dhoable@gmail.com>
2024-07-03 08:48:50 +0700

    add HomeScreen view

--- app/src/main/java/com/mridhop/easppb/ui/view/HomeScreen.kt
+++ app/src/main/java/com/mridhop/easppb/ui/view/HomeScreen.kt
@@ -2,15 +2,21 @@ package com.mridhop.easppb.ui.view
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,15 +40,37 @@ fun HomeScreen(
                 .background(color = Color(0xFF3E9EED))
         )
 
-        Column {
-            Row {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 24.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
                 Image(
                     painter = painterResource(id = R.drawable.isaku_logo),
-                    contentDescription = null
+                    contentDescription = null,
+                    modifier = Modifier.size(64.dp)
                 )
 
                 Row {
-
+                    Image(
+                        painter = painterResource(id = R.drawable.notification),
+                        contentDescription = null,
+                        colorFilter = ColorFilter.tint(Color.White),
+                        modifier = Modifier.size(28.dp)
+                    )
+                    Spacer(modifier = Modifier.size(16.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.baseline_person_24),
+                        contentDescription = null,
+                        modifier = Modifier.size(28.dp)
+                    )
                 }
             }
 
